de7c29d6813c3997d529c482fa2588e1cf8d7502
osakhsa <osakhsa3@gmail.com>
2020-12-15 22:31:14 +0300

    I can write some sort algorithm

--- sort.py
+++ sort.py
@@ -2,6 +2,6 @@ def bubblesort(tosort):
     n = len(tosort)
     for i in range(n-1, 0, -1):
         for el in range(i):
-            if tosort[i] > tosort[i+1]:
-                tosort[i], tosort[i+1] = tosort[i+1], tosort[i]
+            if tosort[el] > tosort[el+1]:
+                tosort[el], tosort[el+1] = tosort[el+1], tosort[el]
     return(tosort)
